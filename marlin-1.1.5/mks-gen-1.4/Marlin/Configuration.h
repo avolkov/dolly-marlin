0ab8de50b092c4ec9a9dbbec6deb5824fa83cabe
Alex Volkov <alex@flamy.ca>
2017-12-01 21:51:36 -0500

    Updated extruder stepping settings for new motor/chip

--- marlin-1.1.5/mks-gen-1.4/Marlin/Configuration.h
+++ marlin-1.1.5/mks-gen-1.4/Marlin/Configuration.h
@@ -508,7 +508,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 4000, 143 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 4000, 180 }
 
 /**
  * Default Max Feed Rate (mm/s)
