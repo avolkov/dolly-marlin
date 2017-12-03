f2e4372d3f0c591dd9c3752ccafc141df87c6976
Alex Volkov <alex@flamy.ca>
2017-12-03 18:41:43 -0500

    Update bed temp hysteresis

--- marlin-1.1.5/mks-gen-1.4/Marlin/Configuration.h
+++ marlin-1.1.5/mks-gen-1.4/Marlin/Configuration.h
@@ -305,7 +305,7 @@
 
 // Bed temperature must be close to target for this long before M190 returns success
 #define TEMP_BED_RESIDENCY_TIME 10  // (seconds)
-#define TEMP_BED_HYSTERESIS 5       // (degC) range of +/- temperatures considered "close" to the target one
+#define TEMP_BED_HYSTERESIS 6       // (degC) range of +/- temperatures considered "close" to the target one
 #define TEMP_BED_WINDOW     1       // (degC) Window around target to start the residency timer x degC early.
 
 // The minimal temperature defines the temperature below which the heater will not be enabled It is used
