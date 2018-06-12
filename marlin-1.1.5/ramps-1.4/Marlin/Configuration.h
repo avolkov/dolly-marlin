ca81791a477e1af4f4dd9723af083267c9250f19
Alex Volkov <alex@flamy.ca>
2018-06-11 21:22:31 -0400

    Adjusted X bed size to a real value -- 210mm

--- marlin-1.1.5/ramps-1.4/Marlin/Configuration.h
+++ marlin-1.1.5/ramps-1.4/Marlin/Configuration.h
@@ -765,7 +765,7 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 215
+#define X_BED_SIZE 210
 #define Y_BED_SIZE 209
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
