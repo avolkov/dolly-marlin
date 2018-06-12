be5b5fcacc3543f7a80c09dfa3675098ecc2d443
Alex Volkov <alex@flamy.ca>
2018-06-11 21:11:41 -0400

    Updated max position to calibrate z motors.

--- marlin-1.1.5/ramps-1.4/Marlin/Configuration.h
+++ marlin-1.1.5/ramps-1.4/Marlin/Configuration.h
@@ -774,7 +774,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 180
+#define Z_MAX_POS 195
 
 // If enabled, axes won't move below MIN_POS in response to movement commands.
 #define MIN_SOFTWARE_ENDSTOPS
