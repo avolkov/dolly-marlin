940425116d343c3879fce2936601ee57419eded7
Alex Volkov <alex@flamy.ca>
2018-06-25 23:20:08 -0400

    Tewaking feed rate for 12v

--- marlin-1.1.5/mks-gen-1.4/Marlin/Configuration.h
+++ marlin-1.1.5/mks-gen-1.4/Marlin/Configuration.h
@@ -767,7 +767,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 190
+#define Z_MAX_POS 195
 
 // If enabled, axes won't move below MIN_POS in response to movement commands.
 //#define MIN_SOFTWARE_ENDSTOPS
--- marlin-1.1.8/ramps-1.4/Marlin/Configuration.h
+++ marlin-1.1.8/ramps-1.4/Marlin/Configuration.h
@@ -536,7 +536,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 200, 200, 200, 50 }
+#define DEFAULT_MAX_FEEDRATE          { 200, 200, 800, 50 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -1040,7 +1040,7 @@
 
 // Homing speeds (mm/m)
 #define HOMING_FEEDRATE_XY (50*60)
-#define HOMING_FEEDRATE_Z  (4*60)
+#define HOMING_FEEDRATE_Z  (20*60)
 
 // @section calibrate
 
