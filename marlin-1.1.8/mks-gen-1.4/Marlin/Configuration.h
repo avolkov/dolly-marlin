9a1fc80e385cd87517a9144c52edebbfee20588e
Alex Volkov <alex@flamy.ca>
2021-01-08 20:23:43 -0500

    Getting better temperature values.

--- marlin-1.1.8/mks-gen-1.4/Marlin/Configuration.h
+++ marlin-1.1.8/mks-gen-1.4/Marlin/Configuration.h
@@ -1153,12 +1153,12 @@
 // @section temperature
 
 // Preheat Constants
-#define PREHEAT_1_TEMP_HOTEND 180
-#define PREHEAT_1_TEMP_BED     70
+#define PREHEAT_1_TEMP_HOTEND 210
+#define PREHEAT_1_TEMP_BED     65
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    110
+#define PREHEAT_2_TEMP_HOTEND 230
+#define PREHEAT_2_TEMP_BED     80
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 /**
