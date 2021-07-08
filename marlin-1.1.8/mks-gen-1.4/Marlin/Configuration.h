19588237df7e05dc05ca35cf019eac530c9b2a50
Alex Volkov <alex@flamy.ca>
2021-07-07 20:44:56 -0400

    Updating config timestamp

--- marlin-1.1.8/mks-gen-1.4/Marlin/Configuration.h
+++ marlin-1.1.8/mks-gen-1.4/Marlin/Configuration.h
@@ -74,7 +74,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_CONFIG_H_AUTHOR "(Alex Volkov, 2021 July 04 / mks-gen-1.4 / 1934 )" // Who made the
+#define STRING_CONFIG_H_AUTHOR "(Alex Volkov, 2021 July 07 / mks-gen-1.4 / 2043 )" // Who made the
 #define SHOW_BOOTSCREEN
 #define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
 #define STRING_SPLASH_LINE2 WEBSITE_URL         // will be shown during bootup in line 2
