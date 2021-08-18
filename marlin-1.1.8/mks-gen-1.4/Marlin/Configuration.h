4b614f0c4df8756081d482198dc4b07c5e7ba833
Alex Volkov <alex@flamy.ca>
2021-08-18 13:43:24 -0400

    Testing paper z alignment offset.

--- marlin-1.1.8/mks-gen-1.4/Marlin/Configuration.h
+++ marlin-1.1.8/mks-gen-1.4/Marlin/Configuration.h
@@ -699,7 +699,7 @@
 */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 19  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 6  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.1   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000 
