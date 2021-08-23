61ca5185f428411bcb422d319ee6adb7d2047642
Alex Volkov <alex@flamy.ca>
2021-08-22 23:06:59 -0400

    Updating inset for ubl mesh

--- marlin-1.1.9/mks-gen-1.4/Marlin/Configuration_adv.h
+++ marlin-1.1.9/mks-gen-1.4/Marlin/Configuration_adv.h
@@ -843,10 +843,10 @@
 
 #if ENABLED(MESH_BED_LEVELING) || ENABLED(AUTO_BED_LEVELING_UBL)
   // Override the mesh area if the automatic (max) area is too large
-  //#define MESH_MIN_X MESH_INSET
-  //#define MESH_MIN_Y MESH_INSET
-  //#define MESH_MAX_X X_BED_SIZE - (MESH_INSET)
-  //#define MESH_MAX_Y Y_BED_SIZE - (MESH_INSET)
+  #define MESH_MIN_X 15
+  #define MESH_MIN_Y 38
+  #define MESH_MAX_X 182
+  #define MESH_MAX_Y 199
 #endif
 
 // @section extras
