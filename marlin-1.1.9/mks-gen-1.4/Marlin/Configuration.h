bb4032bb244b7756df9299ca294d93ec351d6d66
Alex Volkov <alex@flamy.ca>
2021-09-01 23:00:10 -0400

    Add scroll long names feature

--- marlin-1.1.9/mks-gen-1.4/Marlin/Configuration.h
+++ marlin-1.1.9/mks-gen-1.4/Marlin/Configuration.h
@@ -81,7 +81,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_CONFIG_H_AUTHOR "(Alex Volkov, 2021 September 01 / mks-gen-1.4 / 1323 )" // Who made the
+#define STRING_CONFIG_H_AUTHOR "(Alex Volkov, 2021 September 01 / mks-gen-1.4 / 2259 )" // Who made the
 #define SHOW_BOOTSCREEN
 #define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
 #define STRING_SPLASH_LINE2 "21/09/01 mks-gen1.4"         // will be shown during bootup in line
