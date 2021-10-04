af9789946cab63a33b5120045b871d382bb0cd79
Alex Volkov <alex@flamy.ca>
2021-10-03 23:40:18 -0400

    Adding debugging info

--- marlin-2.0.9.1/skr-14-turbo/build_copy.sh
+++ marlin-2.0.9.1/skr-14-turbo/build_copy.sh
@@ -8,5 +8,7 @@ fi
 rm /media/alex/disk/*
 pio run
 [ $? -ne 0 ] && exit 2
-cp .pio/build/LPC1769/firmware.bin /media/alex/disk/
+cp -v .pio/build/LPC1769/firmware.bin /media/alex/disk/
+
 umount /media/alex/disk
+echo "unmounted sd card"
