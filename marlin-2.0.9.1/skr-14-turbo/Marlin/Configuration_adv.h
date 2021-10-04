7e82cfacefabd2bc0a7d9f94dd492a19739e79e2
Alex Volkov <alex@flamy.ca>
2021-10-04 18:17:53 -0400

    Adjusting sensorless homing torque to half of max power rating.

--- marlin-2.0.9.1/skr-14-turbo/Marlin/Configuration_adv.h
+++ marlin-2.0.9.1/skr-14-turbo/Marlin/Configuration_adv.h
@@ -2562,7 +2562,7 @@
 
   #if AXIS_IS_TMC(X)
     #define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
+    #define X_CURRENT_HOME  400        // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     32        // 0..256
     #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1        // -1..0: Not chained. 1: MCU MOSI connected. 2: Next in chain, ...
