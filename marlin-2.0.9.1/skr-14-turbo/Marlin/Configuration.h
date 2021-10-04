fb87e8267471c9d9626b2f39b44d70597fc3a8b1
Alex Volkov <alex@flamy.ca>
2021-10-04 19:06:18 -0400

    Updating extruder settings (mA and steps_per_unit from skr-bear-marlin)

--- marlin-2.0.9.1/skr-14-turbo/Marlin/Configuration.h
+++ marlin-2.0.9.1/skr-14-turbo/Marlin/Configuration.h
@@ -926,7 +926,12 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 800, 500 }
+/*
+See SKR-bear-Marlin -- Prusa_SKR_Conditionals
+PRUSA_SKR_E_STEPPER == 1 // Stock 1.8°
+*/
+
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 800, 280 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1331,7 +1336,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
+#define INVERT_E0_DIR true
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
--- marlin-2.0.9.1/skr-14-turbo/Marlin/Configuration_adv.h
+++ marlin-2.0.9.1/skr-14-turbo/Marlin/Configuration_adv.h
@@ -2660,7 +2660,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      800
+    #define E0_CURRENT       1200
     #define E0_MICROSTEPS    32
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
