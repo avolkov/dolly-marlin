67d302036c941102ba1c373ecc73c685e4d3c37b
Alex Volkov <alex@flamy.ca>
2021-10-05 11:48:08 -0400

    Using SKR-Bear-marlin values for bed pid

--- marlin-2.0.9.1/skr-14-turbo/Marlin/Configuration.h
+++ marlin-2.0.9.1/skr-14-turbo/Marlin/Configuration.h
@@ -649,19 +649,12 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-  // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
-  // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
-
-
   //Possible settings for MK52 prusa bed from -- https://github.com/codiac2600/SKR-Bear-Marlin/blob/prusa-skr-aio-2.0.x/Marlin/Prusa_SKR_Configuration.h
-  /*
+
   #define PRUSA_SKR_DEFAULT_bedKp  28.13
   #define PRUSA_SKR_DEFAULT_bedKi   2.09
   #define PRUSA_SKR_DEFAULT_bedKd 483.07
-  */
+
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
 
