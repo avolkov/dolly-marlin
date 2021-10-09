64e3cb403572dabe8294ec94495ba09d3e6e5bf1
Alex Volkov <alex@flamy.ca>
2021-10-09 19:38:14 -0400

    Defining FILAMENT_RUNOUT_DISTANCE as a workaround for fil runout not working.

--- marlin-2.0.9.2/skr-14-turbo/Marlin/Configuration.h
+++ marlin-2.0.9.2/skr-14-turbo/Marlin/Configuration.h
@@ -1515,7 +1515,7 @@ PRUSA_SKR_E_STEPPER == 1 // Stock 1.8°
   // After a runout is detected, continue printing this length of filament
   // before executing the runout script. Useful for a sensor at the end of
   // a feed tube. Requires 4 bytes SRAM per sensor, plus 4 bytes overhead.
-  //#define FILAMENT_RUNOUT_DISTANCE_MM 25
+  #define FILAMENT_RUNOUT_DISTANCE_MM 5
 
   #ifdef FILAMENT_RUNOUT_DISTANCE_MM
     // Enable this option to use an encoder disc that toggles the runout pin
