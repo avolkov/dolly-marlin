979de1f7c8e9335b34d3c934f19412269a97eb3f
Alex Volkov <alex@flamy.ca>
2021-10-13 21:42:05 -0400

    Updated line version.

--- marlin-2.0.9.2/skr-14-turbo/Marlin/Configuration.h
+++ marlin-2.0.9.2/skr-14-turbo/Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Alex Volkov, 2021-10-02)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Alex Volkov, 2021-10-13)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
