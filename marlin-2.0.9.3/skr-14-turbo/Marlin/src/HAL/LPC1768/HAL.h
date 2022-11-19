44464d0a8cdae0f78660519bc51d752c72bee508
Alex Volkov <alex@flamy.ca>
2022-11-18 23:04:00 -0500

    Temp fix

--- marlin-2.0.9.3/skr-14-turbo/Marlin/src/HAL/LPC1768/HAL.h
+++ marlin-2.0.9.3/skr-14-turbo/Marlin/src/HAL/LPC1768/HAL.h
@@ -137,7 +137,7 @@ int freeMemory();
 //
 
 #define ADC_MEDIAN_FILTER_SIZE (23) // Higher values increase step delay (phase shift),
-                                    // (ADC_MEDIAN_FILTER_SIZE + 1) / 2 sample step delay (12 samples @ 500Hz: 24ms phase shift)
+                                    // (ADC_MEDIAN_FILTER_SIZE + 1) / 2 sample step delay (12 nsamples @ 500Hz: 24ms phase shift)
                                     // Memory usage per ADC channel (bytes): (6 * ADC_MEDIAN_FILTER_SIZE) + 16
                                     // 8 * ((6 * 23) + 16 ) = 1232 Bytes for 8 channels
 
