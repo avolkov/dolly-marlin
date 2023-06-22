f821826bfe922352c3e6711c3fef5450435ce07a
Alex Volkov <alex@flamy.ca>
2023-06-22 14:10:39 +0700

    Fixed steps per mm for e

--- marlin-2.1.2.1/skr-14-turbo/Marlin/Configuration.h
+++ marlin-2.1.2.1/skr-14-turbo/Marlin/Configuration.h
@@ -1214,7 +1214,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 800, 280 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 800, 140 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
