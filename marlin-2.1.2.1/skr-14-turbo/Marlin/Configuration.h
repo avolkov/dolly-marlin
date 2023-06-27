20b4b3055563d4125fc84e9c2ae24d70de4af12e
Alex Volkov <alex@flamy.ca>
2023-06-27 11:37:21 +0700

    fix

--- marlin-2.1.2.1/skr-14-turbo/Marlin/Configuration.h
+++ marlin-2.1.2.1/skr-14-turbo/Marlin/Configuration.h
@@ -1984,7 +1984,7 @@
 /**
  * Auto-leveling needs preheating
  */
-#define PREHEAT_BEFORE_LEVELING
+//#define PREHEAT_BEFORE_LEVELING
 #if ENABLED(PREHEAT_BEFORE_LEVELING)
   #define LEVELING_NOZZLE_TEMP 175   // (°C) Only applies to E0 at this time
   #define LEVELING_BED_TEMP     60
